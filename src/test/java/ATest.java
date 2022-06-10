311fb6d1910f5d4258c9a6379e5d32bd0a5e9141
soda <lbprivateacc@gmail.com>
2022-06-11 00:28:45 +0800

    leetcode

--- src/test/java/ATest.java
+++ src/test/java/ATest.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
 
+import java.util.*;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ATest {
 
 
@@ -21,5 +26,44 @@ public class ATest {
         System.out.println(i == i1);
         System.out.println(i2 == i4);
         System.out.println(i == i5);
+    }
+
+    @Test
+    public void outOfTime() throws InterruptedException {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                throw new RuntimeException();
+            }
+        }, 1);
+        TimeUnit.SECONDS.sleep(1);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                throw new RuntimeException();
+            }
+        }, 1);
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    @Test
+    public void dp() {
+        int[] G = new int[4];
+        G[0] = 1;
+        G[1] = 1;
+        for(int i = 2; i <= 3; i++){
+            for(int j = 1; j <= i; j ++){
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        System.out.println("fafa");
+    }
+
+    @Test
+    public void remove() {
+
+        System.out.println("leetcode".compareTo("coding"));
+
     }
 }
